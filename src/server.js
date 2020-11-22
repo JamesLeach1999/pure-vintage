edde4cd7d4dae6dca1364a752f1ba58913e85b09
james_leach1999 <james.leach1999@gmail.com>
2020-11-22 19:16:06 +0000

    still trying heroku, surprise surprise

--- src/server.js
+++ src/server.js
@@ -88,7 +88,7 @@ app.use('/', authRoute);
 app.use('/', postRoute);
 app.use('/', orderRoute);
 
-app.use(express.static('work/build'));
+app.use(express.static('../work'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('work/build'));

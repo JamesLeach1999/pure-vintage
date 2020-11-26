6545c9b007a2b7975265ae09f540fe1529387966
james_leach1999 <james.leach1999@gmail.com>
2020-11-26 17:43:06 +0000

    this is way harder than i thoought

--- work/src/components/RefundProducts.js
+++ work/src/components/RefundProducts.js
@@ -1,105 +1,89 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import Axios from "axios"
 // function ID(id) {
 //   return id;
 // }
-class Product extends Component {
-  constructor() {
-    super();
-    this.state = { orders: [], images: [], data: [] };
-  }
+const Product = () => {
+  const {id} = useParams()
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [order, setOrder] = useState()
 
-  async componentDidMount() {
-    const profile = await Axios.get(`/me?id=${localStorage.getItem("user")}`);
+    const getOrderProducts = async () => {
+      const profile = await Axios.get(`/me?id=${localStorage.getItem("user")}`);
     console.log(profile);
     if (!profile) {
       window.location.replace("/store");
     } else {
       try {
         console.log();
-        const response = await fetch(`/orderProducts?id=${this.props.id}`);
+        const response = await fetch(`/orderProducts?id=${id}`);
         const json = await response.json();
-        console.log(this.props.id);
+        console.log(id);
         var allOrders = [];
         console.log(json);
-        json.names.orderItems.map((order) => {
-          console.log(order);
-          if (order !== null) {
-            allOrders.push(order);
-          }
-        });
+        setProducts(json.orderItems)
+        setOrder(json.names)
 
-        this.setState({ data: JSON.parse(allOrders) });
-        console.log(this.state.data);
+        // console.log(this.state.data);
 
-        var it = [];
-        var sumPrice = [];
-        var sum;
-        this.state.data.map((items) => {
-          it.push(JSON.parse(items.orderItems[0]));
-          // console.log(it)
-          it.map((price) => {
-            console.log(price);
-            var t = [];
-            price.map((r) => {
-              t.push(r.product.price);
-              console.log(t);
-            });
-            sum = t.reduce(function (a, b) {
-              return a + b;
-            }, 0);
-            console.log(sum);
-            t = [];
-          });
-          sumPrice.push(sum);
-        });
-        this.setState({ orders: it });
-        this.setState({ sum: sumPrice });
-        console.log(this.state.data);
-        console.log(this.state.orders);
+        // var it = [];
+        // var sumPrice = [];
+        // var sum;
+        console.log(products)
+        setLoading(false)
+        // console.log(this.state.data);
+        // console.log(this.state.orders);
+
       } catch (error) {
         console.log(this.props.id);
         console.log(error);
-      }
-    }
+      }}
+    
   }
 
-  render() {
+  useEffect(() => {
+    getOrderProducts()
+  }, [loading])
+
     return (
       <div className="testimonial">
         <div className="small-container">
           <div class="row">
             {/* <h1>{this.props.id}</h1> */}
-            {this.state.data.map((product, i) => {
+            {products.map((product, i) => {
               return (
-                <Link to={`/refundPage/${this.state.data[i]._id}`}>
-                  <tr>
-                    <td>
-                      <img
-                        src={`/assets/${this.state.data[i].image[0]}`}
-                        alt=""
-                      />
-                      <p>{this.state.data[i].name}</p>
-                    </td>
-                    <td>
-                      <ul>
-                        <li>{this.state.data[i].shipping.address}</li>
-                        <li>{this.state.data[i].shipping.city}</li>
-                        <li>{this.state.data[i].shipping.postcode}</li>
-                      </ul>
-                    </td>
-                    <td>{this.state.data[i].updatedAt}</td>
-                    <td>£ {this.state.sum[i]}</td>
-                  </tr>
-                </Link>
+                <div className="col-4">
+                  <img src={`${product.image}`} alt="" />
+                  <h4>{product.name}</h4>
+                  
+                  <p>{product.price}</p>
+                  <form action="/refundSingle" method="post">
+                    <input
+                      type=""
+                      name="productId"
+                      value={product._id}
+                      hidden
+                    />
+                    <input type="" name="id" value={order._id} hidden />
+                    <input
+                      type=""
+                      name="amount"
+                      value={product.price}
+                      hidden
+                    />
+                    <input type="" name="intent" value={order.intent} hidden />
+                    <button type="submit">Refund Product?</button>
+                  </form>
+                </div>
               );
             })}
           </div>
         </div>
       </div>
     );
-  }
+  
 }
 
 export default Product;

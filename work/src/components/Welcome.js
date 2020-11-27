db4477d2929b74cfd32a40d7c95e5e0263a38db2
james_leach1999 <james.leach1999@gmail.com>
2020-11-27 21:12:21 +0000

    this is way harder than i thoought

--- work/src/components/Welcome.js
+++ work/src/components/Welcome.js
@@ -2,38 +2,44 @@ import React, { useEffect } from "react";
 import defaultImage from "../assets/Logo_blue_text.jpg";
 import "../css/slide.css";
 const Welcome = () => {
-  var slideIndex = 1;
-  showSlides(slideIndex);
 
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
 
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides((slideIndex = n));
-  }
+  useEffect(() => {
 
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-      slideIndex = 1;
+    var slideIndex = 1;
+    showSlides(slideIndex);
+  
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides((slideIndex += n));
     }
-    if (n < 1) {
-      slideIndex = slides.length;
+  
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides((slideIndex = n));
     }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  
+    function showSlides(n) {
+      var i;
+      console.log(document.getElementsByClassName("mySlides"));
+      var slides = document.getElementsByClassName("mySlides")[0];
+      var dots = document.getElementsByClassName("dot")[0];
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
+  },[])
 
   return (
     <>

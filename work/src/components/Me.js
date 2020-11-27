b6f59011543d389362e025a7b8faca146fd1616f
james_leach1999 <james.leach1999@gmail.com>
2020-11-27 18:01:55 +0000

    this is way harder than i thoought

--- work/src/components/Me.js
+++ work/src/components/Me.js
@@ -87,7 +87,7 @@ const Me = () => {
                 return (
                   <tr>
                     <td>
-                      <Link to={`/orderProducts/${product._id}}`}>
+                      <Link to={`/orderProducts/${product._id}`}>
                         <img src={orders[i][0].product.image[0]} alt="" />
                       </Link>
                       {/* <img src={`/assets/${product.images[0]}`} alt="" /> */}

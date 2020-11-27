bd5e2700b013d157bc14e8ab84f3ea5202f92c6c
james_leach1999 <james.leach1999@gmail.com>
2020-11-27 21:36:14 +0000

    this is way harder than i thoought

--- work/src/components/Welcome.js
+++ work/src/components/Welcome.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import defaultImage from "../assets/Logo_blue_text.jpg";
-import Car from "./Car"
+import { UncontrolledCarousel } from "reactstrap";
 
+const items = [
+  {
+    src:
+      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512786/img_nature_wide_q8vuhx.jpg",
+    altText: "Slide 1",
+    caption: "Slide 1",
+    header: "Slide 1 Header",
+    key: "1",
+  },
+  {
+    src:
+      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512795/img_snow_wide_ys9co2.jpg",
+    altText: "Slide 2",
+    caption: "Slide 2",
+    header: "Slide 2 Header",
+    key: "2",
+  },
+  {
+    src:
+      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg",
+    altText: "Slide 3",
+    caption: "Slide 3",
+    header: "Slide 3 Header",
+    key: "3",
+  },
+];
 const Welcome = () => {
+
+
     return (
       <>
         <div class="row header">
-          <Car/>
+          <UncontrolledCarousel items={items} />
         </div>
       </>
     );

54803a8dbb614a8a9d785380d188f1267f80ee32
james_leach1999 <james.leach1999@gmail.com>
2020-11-28 14:36:01 +0000

    think im getting the hang of this

--- work/src/components/Welcome.js
+++ work/src/components/Welcome.js
@@ -2,39 +2,28 @@ import React from 'react'
 import defaultImage from "../assets/Logo_blue_text.jpg";
 import { UncontrolledCarousel } from "reactstrap";
 import Car from "./Car"
-const items = [
-  {
-    src:
-      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512786/img_nature_wide_q8vuhx.jpg",
-    altText: "Slide 1",
-    caption: "Slide 1",
-    header: "Slide 1 Header",
-    key: "1",
-  },
-  {
-    src:
-      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512795/img_snow_wide_ys9co2.jpg",
-    altText: "Slide 2",
-    caption: "Slide 2",
-    header: "Slide 2 Header",
-    key: "2",
-  },
-  {
-    src:
-      "https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg",
-    altText: "Slide 3",
-    caption: "Slide 3",
-    header: "Slide 3 Header",
-    key: "3",
-  },
-];
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 const Welcome = () => {
 
 
     return (
       <>
         <div class="row header">
-          <Car/>
+          <AliceCarousel autoPlay autoPlayInterval="3000">
+            <img
+              src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg"
+              className="sliderimg"
+            />
+            <img
+              src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg"
+              className="sliderimg"
+            />
+            <img
+              src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg"
+              className="sliderimg"
+            />
+          </AliceCarousel>
         </div>
       </>
     );

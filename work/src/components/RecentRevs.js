79eb63e0e9d33dc768aba833df58e689676f6640
james_leach1999 <james.leach1999@gmail.com>
2020-11-29 16:55:40 +0000

    think im getting the hang of this

--- work/src/components/RecentRevs.js
+++ work/src/components/RecentRevs.js
@@ -12,12 +12,11 @@ class Reviews extends Component {
 
   async componentDidMount() {
     try {
-      const p = await fetch("/recentReviews")
-      const pJson = await p.json()
-      console.log(pJson)
-      this.setState({data: pJson.name})
-
-      console.log(this.state.data)
+      const p = await fetch("/recentReviews");
+      const pJson = await p.json();
+      this.setState({ data: pJson.name });
+      this.setState({ images: pJson.images });
+      console.log(this.state.data);
     } catch (error) {
       console.log(this.props);
       console.log(error);
@@ -26,19 +25,24 @@ class Reviews extends Component {
 
   render() {
     return (
-        <div>
-            
-      {/* {this.state.data.map((review) => {
-          <div key={review._id}>
-            <h3 style={{ paddingBottom: "15px", marginTop: "-30px" }}>
-              {review.name}
-            </h3>
-            <p>{review.comment}</p>
-            <p style={{ paddingTop: "15px" }}>{review.rating}/5</p>
-            {/* <img src="assets/shoes1.jpg" alt="" /> */}
-          {/* </div>; */}
-    {/* // })} */}
-    </div>
+      <div class="testimonial">
+        <div class="small-container">
+          Reviews:
+          <div class="row">
+            {this.state.data.map((review, i) => {
+              <div key={review._id}>
+                <img src={this.state.images[i]} alt="" />
+                <h3 style={{ paddingBottom: "15px", marginTop: "-30px" }}>
+                  {review.name}
+                </h3>
+                <p>{review.comment}</p>
+                <p style={{ paddingTop: "15px" }}>{review.rating}/5</p>
+                {/* <img src="assets/shoes1.jpg" alt="" /> */}
+              </div>;
+            })}
+          </div>
+        </div>
+      </div>
     );
   }
 }

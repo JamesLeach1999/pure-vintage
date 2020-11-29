ab52c59dfe38dcf775a78e944446b71b5343f1b4
james_leach1999 <james.leach1999@gmail.com>
2020-11-29 14:01:49 +0000

    think im getting the hang of this

--- work/src/components/OtherRevs.js
+++ work/src/components/OtherRevs.js
@@ -10,22 +10,7 @@ class OtherRevs extends Component {
   async componentDidMount() {
     try {
       console.log(this.props.datat)
-      const response = await fetch(`/otherReviews?category=${this.props.category}&id=${this.props.id}`);
-      const json = await response.json();
-      this.setState({ data: json.name[0].reviews });
-      console.log(json.name)
-      console.log(this.state.data);
-      var review
-      var t = []
-      json.name.map((rev) => {
-        t.push(rev.review)
-      })
-
-      console.log(review);
-
-      this.setState({ review: review });
-
-      console.log(this.state.review);
+      
     } catch (error) {
       console.log(this.props.id);
       console.log(error);
@@ -38,12 +23,12 @@ class OtherRevs extends Component {
         <i class="fa fa-quote-left"></i>
         {this.state.review.map((t) => {
           return (
-            <div key={t._id}>
+            <div key={this.props.datat._id}>
               <h3 style={{ paddingBottom: "15px", marginTop: "-30px" }}>
-                {t.name}
+                {this.props.datat.name}
               </h3>
-              <p>{t.comment}</p>
-              <p style={{ paddingTop: "15px" }}>{t.rating}/5</p>
+              <p>{this.props.datat.comment}</p>
+              <p style={{ paddingTop: "15px" }}>{this.props.datat.rating}/5</p>
               {/* <img src="assets/shoes1.jpg" alt="" /> */}
             </div>
           );

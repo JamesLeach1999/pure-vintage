f32cacc99446358e42bcf50550a2f648e43353a1
james_leach1999 <james.leach1999@gmail.com>
2020-11-29 16:29:45 +0000

    think im getting the hang of this

--- src/routers/product.js
+++ src/routers/product.js
@@ -542,14 +542,14 @@ router.get('/recentReviews', async (req, res) => {
   orders.forEach(async (o) => {
     const oProducts = JSON.parse(o.orderItems);
     console.log('num');
-    // console.log(oProducts[0].product);
-    const product = await Product.findById({_id: oProducts[0].product._id})
-console.log(product)
-    if(product.reviews[0] !== undefined || product.reviews[0] !== []){
-      console.log(product.reviews[0]);
-      review.push(product.reviews[0])
-    }
-    
+    console.log(oProducts[0].product.reviews);
+    const product = await Product.findById({ _id: oProducts[0].product._id });
+    console.log('num1');
+    console.log(product);
+    // if (product.reviews[0] !== undefined || product.reviews[0] !== []) {
+    console.log(product.reviews[0]);
+    review.push(product.reviews[0]);
+    // }
   });
   console.log('yes');
   console.log(review);

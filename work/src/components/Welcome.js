58825c1653a34f2513f78199f44102b24abf64c7
james_leach1999 <james.leach1999@gmail.com>
2020-11-30 20:24:58 +0000

    think im getting the hang of this

--- work/src/components/Welcome.js
+++ work/src/components/Welcome.js
@@ -9,19 +9,22 @@ const Welcome = () => {
 
     return (
       <>
-        <div class="row header work">
-          <AliceCarousel autoPlay autoPlayInterval="3000" style={{maxWidth: "1896px"}}>
+        <div class="row header work" style={{ maxWidth: "1896px" }}>
+          <AliceCarousel autoPlay autoPlayInterval="3000">
             <img
               src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg"
-              className="sliderimg" alt=""
+              className="sliderimg"
+              alt=""
             />
             <img
               src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512795/img_snow_wide_ys9co2.jpg"
-              className="sliderimg" alt=""
+              className="sliderimg"
+              alt=""
             />
             <img
               src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512786/img_nature_wide_q8vuhx.jpg"
-              className="sliderimg" alt=""
+              className="sliderimg"
+              alt=""
             />
           </AliceCarousel>
         </div>

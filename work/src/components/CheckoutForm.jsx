c15f3a3bb3c86ca83b4afc9bd27222dc8a53fc24
james_leach1999 <james.leach1999@gmail.com>
2020-12-03 17:33:25 +0000

    think im getting the hang of this

--- work/src/components/CheckoutForm.jsx
+++ work/src/components/CheckoutForm.jsx
@@ -130,15 +130,7 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
 
   return (
     <form onSubmit={handleFormSubmit}>
-      <Row style={{width: "350px",
-      margin: "30px auto",
-      boxShadow:
-        "0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08), inset 0 1px 0 #000000",
-      borderRadius: "4px",
-      border: "red",
-      backgrounColor: "#ffffff",
-      position: "relative",
-    }}>
+      <Row>
         <BillingDetailsFields />
       </Row>
       <Row>

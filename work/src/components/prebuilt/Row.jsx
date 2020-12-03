0a05eb4530e01175bbd095a3bd596e04e7905d35
james_leach1999 <james.leach1999@gmail.com>
2020-12-03 17:43:58 +0000

    think im getting the hang of this

--- work/src/components/prebuilt/Row.jsx
+++ work/src/components/prebuilt/Row.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 
 const Row = styled.div`
-  width: 300px;
+  width: 350px;
   margin: 30px auto;
   box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
     inset 0 1px 0 #000000;
@@ -10,6 +10,9 @@ const Row = styled.div`
   border: #000000;
   background-color: #ffffff;
   position: relative;
+  @media only screen and (max-width: 600px) {
+    width: 250px;
+  }
 `;
 
 export default Row;

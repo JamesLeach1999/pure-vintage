f83d7e1e8a7785ad886969b7b9ef4e4552c50ec7
james_leach1999 <james.leach1999@gmail.com>
2020-12-07 10:51:02 +0000

    product page carousel

--- work/src/components/PicCard.js
+++ work/src/components/PicCard.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 
 const PicCard = ({ image }) => {
   
-  return <img src={`${image}`} alt="shit"></img>;
+  return <img className="card" src={`${image}`} alt="shit"></img>;
 };
 
 export default PicCard;

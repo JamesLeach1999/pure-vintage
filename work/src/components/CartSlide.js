c91b59961681a005e2279dae8a6862d4841d9b8f
james_leach1999 <james.leach1999@gmail.com>
2020-12-08 18:09:21 +0000

    product page carousel

--- work/src/components/CartSlide.js
+++ work/src/components/CartSlide.js
@@ -1,30 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState, useRef, useEffect } from "react";
 // import { MenuItems } from "./MenuItems";
 // import  Button  from "../Button";
 import CartProduct from "./CartProduct";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import "../css/Cart.css";
-class Cart extends Component {
-  constructor() {
-    super();
+const Cart = () => {
+  var node1 = useRef();
+  var [cartClicked, setCartClicked] = useState(false);
+  var [data, setData] = useState([]);
+  // constructor() {
+  //   super();
 
-    this.state = {
-      loggedIn: "NOT_LOGGED_IN",
-      user: {},
-      admin: false,
-      auth: false,
-      clicked: false,
-      cartClicked: false,
-      data: [],
-    };
-    // updating state
+  //   this.state = {
+  //     loggedIn: "NOT_LOGGED_IN",
+  //     user: {},
+  //     admin: false,
+  //     auth: false,
+  //     clicked: false,
+  //     cartClicked: false,
+  //     data: [],
+  //   };
+  //   // updating state
 
-    this.handleCartClick = this.handleCartClick.bind(this);
-    this.handleCartOutsideClick = this.handleCartOutsideClick.bind(this);
-  }
+  //   this.handleCartClick = this.handleCartClick.bind(this);
+  //   this.handleCartOutsideClick = this.handleCartOutsideClick.bind(this);
+  // }
 
-  getCart = async () => {
+  const getCart = async () => {
     if (sessionStorage.getItem("user")) {
       const url = `/cart1?id=${sessionStorage.getItem("user")}`;
 
@@ -43,7 +46,7 @@ class Cart extends Component {
           }
         });
         console.log(notNull);
-
+        setData([notNull]);
         this.setState({ data: [notNull] });
         // var total = document.getElementById("total")
         // console.log(this.state.data.name.price);
@@ -54,119 +57,85 @@ class Cart extends Component {
       } catch (error) {
         console.log(error);
       }
+    } else {
+      console.log(localStorage)
     }
   };
-  handleCartClick() {
-    if (!this.state.cartClicked) {
+  const handleCartClick = () => {
+    if (!cartClicked) {
       // attach/remove event handler
       console.log("c");
-      document.addEventListener("click", this.handleCartOutsideClick, false);
+      document.addEventListener("click", handleCartOutsideClick, false);
     } else {
       console.log("l");
 
-      document.removeEventListener("click", this.handleCartOutsideClick, false);
+      document.removeEventListener("click", handleCartOutsideClick, false);
     }
 
-    this.setState((prevState) => ({
+    setCartClicked((prevState) => ({
       cartClicked: !prevState.cartClicked,
     }));
-  }
+  };
 
-  handleCartOutsideClick(e) {
+  const handleCartOutsideClick = (e) => {
     // ignore clicks on the component itself
-    if (this.node1.contains(e.target)) {
+    if (node1.contains(e.target)) {
       console.log("thats numberwang");
-      console.log(this.node1);
+      console.log(node1);
       return;
     }
 
-    this.handleCartClick();
-  }
+    handleCartClick();
+  };
 
-  async checkCart(){
-    console.log("workrkrk")
-    return sessionStorage.getItem("unAuthCart")
-  }
+  // async checkCart(){
+  //   console.log("workrkrk")
+  //   sessionStorage.getItem("unAuthCart")
+  // }
 
-  async componentDidMount() {
-    console.log("numberwang")
-    if (sessionStorage.getItem("user")) {
-      const url = `/cart1?id=${sessionStorage.getItem("user")}`;
+  useEffect(() => {
+    getCart()
+  },[]);
 
-      try {
-        // const test = await fetch("http://localhost:9000/store");
-        // console.log(test);
+  return (
+    <nav className="cartItems" ref={(node1) => (node1 = node1)}>
+      <div className="cart-menu-icon" onClick={handleCartClick}>
+        <i
+          className={this.state.cartClicked ? "fas fa-times" : "fas fa-bars"}
+        ></i>
+      </div>
+      <ul
+        id="MenuItems"
+        className={this.state.cartClicked ? "cart-menu active" : "cart-menu"}
+      >
+        {this.state.data.map((products) => {
+          return products.map((product) => {
+            // var tota = product.price
+            // setTotal(tota + tota)
+            // const image = <img alt="" src={require(`./assets/${n.image}`)}/>
+            return (
+              <tr>
+                <Link to={`/product/${product._id}`}>
+                  <CartProduct id={product._id} />
+                </Link>
+                <br />
 
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-
-        var notNull = [];
-        json.cart.map((pro) => {
-          if (pro !== null) {
-            notNull.push(pro);
-          }
-        });
-        console.log(notNull);
-
-        this.setState({ data: [notNull] });
-        // var total = document.getElementById("total")
-        // console.log(this.state.data.name.price);
-
-        // var sum = total.reduce((a, b) => a + b, 0);
-
-        // setTotal(sum);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      var items = await this.checkCart()
-      console.log("yes")
-      console.log(items)
-    }
-  }
-
-  render() {
-    return (
-      <nav className="cartItems" ref={(node1) => (this.node1 = node1)}>
-        <div className="cart-menu-icon" onClick={this.handleCartClick}>
-          <i
-            className={this.state.cartClicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul
-          id="MenuItems"
-          className={this.state.cartClicked ? "cart-menu active" : "cart-menu"}
-        >
-          {this.state.data.map((products) => {
-            return products.map((product) => {
-              // var tota = product.price
-              // setTotal(tota + tota)
-              // const image = <img alt="" src={require(`./assets/${n.image}`)}/>
-              return (
-                <tr>
-                  <Link to={`/product/${product._id}`}>
-                    <CartProduct id={product._id} />
-                  </Link>
-                  <br />
-
-                  <td>{product.size}</td>
-                  <td id="total">{product.price}</td>
-                  <td>
-                    <form action="/cartProduct" method="POST">
-                      <input type="text" value={product._id} name="id" hidden />
-                      <input type="checkbox" />
-                      <button type="submit">Remove?</button>
-                    </form>
-                  </td>
-                </tr>
-              );
-            });
-          })}
-        </ul>
-      </nav>
-    );
-  }
-}
+                <td>{product.size}</td>
+                <td id="total">{product.price}</td>
+                <td>
+                  <form action="/cartProduct" method="POST">
+                    <input type="text" value={product._id} name="id" hidden />
+                    <input type="checkbox" />
+                    <button type="submit">Remove?</button>
+                  </form>
+                </td>
+              </tr>
+            );
+          });
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default Cart;

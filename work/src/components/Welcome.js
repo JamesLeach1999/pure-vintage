2b36aed3a66b830194fe8886c9ad4ae910e6b3bc
james_leach1999 <james.leach1999@gmail.com>
2020-12-09 21:41:23 +0000

    product page carousel

--- work/src/components/Welcome.js
+++ work/src/components/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => {
 
     return (
       <>
-        <div class="row header work" style={{ width:"100vw" }}>
+        <div class="row header work" style={{ width:window.innerWidth }}>
           <AliceCarousel autoPlay autoPlayInterval="3000">
             <img
               src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1606512799/img_lights_wide_hluzf8.jpg"

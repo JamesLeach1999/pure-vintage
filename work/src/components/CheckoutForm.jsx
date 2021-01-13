dffcddf37ffe91812e744b5b765e65981360974e
james_leach1999 <james.leach1999@gmail.com>
2021-01-13 18:47:08 +0000

    delete product  from store on purchase

--- work/src/components/CheckoutForm.jsx
+++ work/src/components/CheckoutForm.jsx
@@ -39,74 +39,70 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
   console.log();
 
   const handleFormSubmit = async (ev) => {
+    ev.preventDefault();
 
-    try {
-      
-      ev.preventDefault();
-  
-      const billingDetails = {
-        name: ev.target.name.value,
-        email: ev.target.email.value,
-        address: {
-          city: ev.target.city.value,
-          line1: ev.target.address.value,
-          postal_code: ev.target.zip.value,
-        },
-      };
-  
-      var cart = JSON.parse(localStorage.getItem("unAuthCart")) || "";
-  
-      // disabe button whilst requesting
-      setProcessingTo(true);
-  
-      // we get price from the input props
-  
-      // create payment intent on server
-      // returns client_secret of payment intent
-  
-      var id = sessionStorage.getItem("user") || billingDetails.email;
-  
-      const { data: clientSecret } = await axios.post("/payment_intents", {
-        amount: price * 100,
-        id: id,
-        address: billingDetails.address.line1,
-        city: billingDetails.address.city,
-        postcode: billingDetails.address.postal_code,
-        cart: cart
-      });
-  
-      const cardElement = elements.getElement(CardElement);
-  
-      // billing associates the details we collect to the payment we're making
-      // use payment method id to conirm payment
-      // need reference to the cardElement we defined earlier
-      // need stripe.js object
-      // create payment method
-      const paymentMethodReq = await stripe.createPaymentMethod({
-        type: "card",
-        card: cardElement,
-        billing_details: billingDetails,
-      });
-  
-      // confirm card payment
-      // payment method id
-      // client secret
-      // get the payment intent back from this
-      console.log(paymentMethodReq)
-      const confirmedCardPayment = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: paymentMethodReq.paymentMethod.id,
-      });
-      await axios.post("/te", {
-        test: confirmedCardPayment,
-        email: billingDetails.email,
-        id: id,
-        cart: cart,
-      });
-      // redirect on checkout if no errors
-      onSuccessfulCheckout("/store");
-    } catch (error) {
-      console.log(error.message)
-    }
+    const billingDetails = {
+      name: ev.target.name.value,
+      email: ev.target.email.value,
+      address: {
+        city: ev.target.city.value,
+        line1: ev.target.address.value,
+        postal_code: ev.target.zip.value,
+      },
+    };
+
+    var cart = JSON.parse(localStorage.getItem("unAuthCart")) || "";
+
+    // disabe button whilst requesting
+    setProcessingTo(true);
+
+    // we get price from the input props
+
+    // create payment intent on server
+    // returns client_secret of payment intent
+
+    var id = sessionStorage.getItem("user") || billingDetails.email;
+
+    const { data: clientSecret } = await axios.post("/payment_intents", {
+      amount: price * 100,
+      id: id,
+      address: billingDetails.address.line1,
+      city: billingDetails.address.city,
+      postcode: billingDetails.address.postal_code,
+      cart: cart,
+    });
+
+    const cardElement = elements.getElement(CardElement);
+
+    // billing associates the details we collect to the payment we're making
+    // use payment method id to conirm payment
+    // need reference to the cardElement we defined earlier
+    // need stripe.js object
+    // create payment method
+    const paymentMethodReq = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+      billing_details: billingDetails,
+    });
+
+    // confirm card payment
+    // payment method id
+    // client secret
+    // get the payment intent back from this
+    console.log(paymentMethodReq);
+    const confirmedCardPayment = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: paymentMethodReq.paymentMethod.id,
+    });
+
+    console.log(confirmedCardPayment)
+    await axios.post("/te", {
+      test: confirmedCardPayment,
+      email: billingDetails.email,
+      id: id,
+      cart: cart,
+    });
+    // redirect on checkout if no errors
+    onSuccessfulCheckout("/store");
   };
   // to display errors, use a try catch and in the catch, set the checkoutError state object
 
@@ -132,7 +128,6 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
 
   return (
     <form onSubmit={handleFormSubmit}>
-      
       <Row>
         <BillingDetailsFields />
       </Row>
@@ -145,7 +140,14 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
       </Row>
       {checkoutError && <CheckoutError>{checkoutError}</CheckoutError>}
       <Row>
-        <SubmitButton disabled={isProcessing} style={{backgroundColor: "whitesmoke", color: "black", borderTop: "none"}}>
+        <SubmitButton
+          disabled={isProcessing}
+          style={{
+            backgroundColor: "whitesmoke",
+            color: "black",
+            borderTop: "none",
+          }}
+        >
           {isProcessing ? "Processing..." : `Pay £${price}`}
         </SubmitButton>
       </Row>

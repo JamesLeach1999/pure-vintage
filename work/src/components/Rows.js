bd6a8a280f88bfa500365e0f99c617a3797e2412
james_leach1999 <james.leach1999@gmail.com>
2021-01-23 18:03:01 +0000

    removing console logs

--- work/src/components/Rows.js
+++ work/src/components/Rows.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Component } from "react";
 import Product from "./Product";
-import Me from '../pages/Me';
+import Me from "../pages/Me";
+import AliceCarousel from "react-alice-carousel";
 
 import { Link } from "react-router-dom";
 // import { useFetch } from "../hooks/useFetch";
@@ -8,18 +9,36 @@ import { Link } from "react-router-dom";
 class Rows extends Component {
   constructor() {
     super();
-    this.state = { data: [], images: require("../assets/cap1.jpg") };
+    this.state = {
+      data: [],
+      images: require("../assets/cap1.jpg"),
+      size: window.innerWidth,
+    };
+
+    this.checkSize = this.checkSize.bind(this);
+  }
+
+  checkSize() {
+    this.setState({ size: window.innerWidth });
   }
 
   async componentDidMount() {
     try {
+      window.addEventListener("resize", this.checkSize);
+
+      if (this.statesize < 600) {
+        this.setState({ size: this.state.size + 13 });
+      } else {
+        this.setState({ size: this.state.size + 25 });
+      }
       const response = await fetch(`/store1`);
-      console.log(response)
+      console.log(response);
       const json = await response.json();
       this.setState({ data: [json.names] });
       console.log(this.state.data);
 
       console.log(this.state.images);
+      window.removeEventListener("resize", this.checkSize);
     } catch (error) {
       console.log(error);
     }
@@ -30,17 +49,19 @@ class Rows extends Component {
       <div className="small-container" style={{ justifyContent: "center" }}>
         <h1>A few of our products</h1>
         <div className="row">
-          {this.state.data.map((products) => {
-            return products.slice(0, 4).map((product) => {
-              // const image = <img alt="" src={require(`./assets/${n.image}`)}/>
-              return (
-                <Link to={`/product/${product._id}`}>
-                  <Product id={product._id} />
-                  {/* <Product/> */}
-                </Link>
-              );
-            });
-          })}
+          <AliceCarousel autoPlay autoPlayInterval="3000">
+            {this.state.data.map((products) => {
+              return products.slice(0, 4).map((product) => {
+                // const image = <img alt="" src={require(`./assets/${n.image}`)}/>
+                return (
+                  <Link to={`/product/${product._id}`}>
+                    <Product className="sliderImg" id={product._id} />
+                    {/* <Product/> */}
+                  </Link>
+                );
+              });
+            })}
+          </AliceCarousel>
         </div>
       </div>
     );

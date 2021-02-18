bad61c71f48a46b3a219c88488d1ffc19f59f2d8
james_leach1999 <james.leach1999@gmail.com>
2021-02-18 08:22:38 +0000

    sorting out dispatches

--- work/src/reducers/orderReducer.js
+++ work/src/reducers/orderReducer.js
@@ -25,40 +25,53 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "SET_SUMS") {
-      var it = [];
-      var sumPrice = [];
-      var sum1;
-      var idk = [];
-      console.log(action.payload)
-      action.payload.map((items) => {
-        console.log("reducer items")
-        // console.log(items)
-        it.push(JSON.parse(items.orderItems));
-        console.log(it);
-        it.map((price) => {
+    var it = [];
+    var sumPrice = [];
+    var sum1;
+    var idk = [];
+    console.log(action.payload);
+
+    for (var i = 0; i < action.payload.length; i++) {
+      console.log("for loop i");
+      console.log(action.payload[i]);
+
+      var items = JSON.parse(action.payload[i].orderItems);
+      for (var j = 0; j < items.length; j++) {
+        console.log("for loop j");
+
+        console.log(items[j]);
+        console.log(items[j].product);
+      }
+    }
+    action.payload.map((items) => {
+      console.log("reducer items");
+      console.log(items);
+      it.push(JSON.parse(items.orderItems));
+      console.log(it);
+      it.map((price) => {
         //   console.log(price);
-          var t = [];
-          price.map((r) => {
-            t.push(r.product.price);
-            console.log(t);
-          });
-          sum1 = t.reduce(function (a, b) {
-            return a + b;
-          }, 0);
-        //   console.log(sum1);
-          t = [];
+        var t = [];
+        price.map((r) => {
+          t.push(r.product.price);
+          console.log(t);
         });
-        sumPrice.push(sum1);
+        sum1 = t.reduce(function (a, b) {
+          return a + b;
+        }, 0);
+        //   console.log(sum1);
+        t = [];
       });
+      sumPrice.push(sum1);
+    });
     //   console.log(sumPrice)
 
     return { ...state, sum: sumPrice };
   }
   if (action.type === "GET_ORDERS") {
     const orderJson = action.payload;
-    var allOrders = []
+    var allOrders = [];
     orderJson.map((order) => {
-    //   console.log(order);
+      //   console.log(order);
       if (order !== null) {
         allOrders.push(order);
       }
@@ -67,17 +80,16 @@ const reducer = (state, action) => {
     return { ...state, data: allOrders };
   }
 
-  if(action.type === "ORDER_ITEMS"){
-      const orderData = action.payload
-      var orderProductData = []
-      for(var i = 0; orderData.length > i; i++){
-          var orderArray = JSON.parse(orderData[i].orderItems[0])
-          console.log(orderArray)
-        for(var j = 0; orderArray.product.length > j; j++){
-            console.log(orderArray.product[j])
-        }
-
+  if (action.type === "ORDER_ITEMS") {
+    const orderData = action.payload;
+    var orderProductData = [];
+    for (var i = 0; orderData.length > i; i++) {
+      var orderArray = JSON.parse(orderData[i].orderItems[0]);
+      console.log(orderArray);
+      for (var j = 0; orderArray.product.length > j; j++) {
+        console.log(orderArray.product[j]);
       }
+    }
     //   if(orderArray.length > 0){
     //     for(var i = 0; orderArray.length > i; i++){
     //         orderArrayData.push(orderArray[i])
@@ -85,7 +97,7 @@ const reducer = (state, action) => {
     //   }
     //   console.log(orderArrayData)
 
-      return {...state, orders: []}
+    return { ...state, orders: [] };
   }
   // have as many different action types as you want. each different action type
   // returs a different updated state, so all updates in one place

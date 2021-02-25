39ea7d74a01867ffcdf9b5756833c6662300a892
james_leach1999 <james.leach1999@gmail.com>
2021-02-25 15:23:34 +0000

    sorting out pagination

--- work/src/components/Nav.js
+++ work/src/components/Nav.js
@@ -23,14 +23,14 @@ import "../css/Navbar.css";
 import CartSlide from "./CartSlide";
 // have to use links like this in the nav
 const Nav = () => {
-  var [loggedIn, setLoggedIn] = useState("NOT_LOGGED_IN")
-  var [user, setUser] = useState({})
-    var [admin, setAdmin] = useState(false)
-  var [auth, setAuth] = useState(false)
-  var [clicked, setClicked] = useState(false)
-  var [cartClicked, setCartClicked] = useState(false)
-  var [data, setData] = useState([])
-  var refContainer = useRef(null)
+  var [loggedIn, setLoggedIn] = useState("NOT_LOGGED_IN");
+  var [user, setUser] = useState({});
+  var [admin, setAdmin] = useState(false);
+  var [auth, setAuth] = useState(false);
+  var [clicked, setClicked] = useState(false);
+  var [cartClicked, setCartClicked] = useState(false);
+  var [data, setData] = useState([]);
+  var refContainer = useRef(null);
   // constructor() {
   //   super();
 
@@ -54,7 +54,7 @@ const Nav = () => {
   //   this.setState({ clicked: !this.state.clicked });
   // };
 
-  var handleLogin = async(data) => {
+  var handleLogin = async (data) => {
     if (data) {
       console.log("thats numberwangssss");
       const work = await Axios.post("/getAuth", {
@@ -66,9 +66,9 @@ const Nav = () => {
       //   auth: true,
       //   admin: work.data.isAdmin,
       // });
-      setLoggedIn("Logged in")
-      setAuth(true)
-      setAdmin(work.data.isAdmin)
+      setLoggedIn("Logged in");
+      setAuth(true);
+      setAdmin(work.data.isAdmin);
       console.log(work.data);
       sessionStorage.setItem("auth", true);
       sessionStorage.setItem("admin", work.data.isAdmin);
@@ -79,40 +79,11 @@ const Nav = () => {
     if (!data) {
       // sessionStorage.setItem("auth", false);
       // sessionStorage.setItem("admin", false);
-      alert("Email or password incorrect")
+      alert("Email or password incorrect");
     }
-  }
+  };
 
-  var handleClick = () => {
-    if (!clicked) {
-      // attach/remove event handler
-      console.log("c");
-      document.addEventListener("click", handleOutsideClick, false);
-    } else {
-      console.log("l");
-
-      document.removeEventListener("click", handleOutsideClick, false);
-    }
-
-    
-    setClicked(!clicked)
-  }
-
-  var handleOutsideClick = (e) => {
-    // ignore clicks on the component itself
-    console.log(refContainer.current.children);
-    console.log(e.target)
-    console.log(clicked)
-    if (refContainer.current.contains(e.target)) {
-      console.log("thats wangernumb");
-
-      alert("outside click")
-
-      return;
-    }
-
-    handleClick();
-  }
+  
 
   var logout = () => {
     sessionStorage.removeItem("auth");
@@ -120,156 +91,182 @@ const Nav = () => {
     sessionStorage.removeItem("user");
 
     // window.location.replace("/store");
+  };
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      var handleClick = () => {
+        if (!clicked) {
+          // attach/remove event handler
+          console.log("c");
+          document.addEventListener("click", handleOutsideClick, false);
+        } else {
+          console.log("l");
+
+          document.removeEventListener("click", handleOutsideClick, false);
+        }
+
+        setClicked(!clicked);
+      };
+
+      var handleOutsideClick = (e) => {
+        // ignore clicks on the component itself
+        console.log(refContainer.current.children);
+        console.log(e.target);
+        console.log(clicked);
+        if (ref.current.contains(e.target)) {
+          console.log("thats wangernumb");
+
+          alert("outside click");
+
+          return;
+        }
+
+        handleClick();
+      };
+    }, [ref]);
   }
 
-    return (
-      <Router>
-        <nav
-          className="NavbarItems"
-          ref={refContainer}
-          style={{ color: "black", backgroundColor: "white" }}
-        >
-          <img
-            src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1611415440/logo_blue_k8rcyp.png"
-            alt=""
-            style={{ zIndex: "-1", width: "60px"}}
-          />
-          <CartSlide style={{ position: "fixed" }} />
-          <div className="menu-icon" onClick={handleClick}>
-            <i
-              style={{ color: "black", width: "75px", height: "75px" }}
-              className={clicked ? "fas fa-times" : "fas fa-bars"}
-            ></i>
-          </div>
-          <ul
-            id="MenuItems"
-            className={clicked ? "nav-menu active" : "nav-menu"}
-          >
-            <Link to="/">
-              <li className="nav-links">Home</li>
-            </Link>
-            <Link to="/store">
-              <li className="nav-links">Store</li>
-            </Link>
+  useOutsideAlerter(refContainer)
 
-            {sessionStorage.getItem("auth") === "true" ? (
-              <li className="nav-links">
-                <Link to="/me">My past orders</Link>
-              </li>
-            ) : (
-              ""
-            )}
-            {sessionStorage.getItem("admin") === "true" ? (
-              <li className="nav-links">
-                <Link to="/manage">Manage</Link>
-              </li>
-            ) : (
-              ""
-            )}
+  return (
+    <Router>
+      <nav
+        className="NavbarItems"
+        ref={refContainer}
+        style={{ color: "black", backgroundColor: "white" }}
+      >
+        <img
+          src="https://res.cloudinary.com/dhey8vvcx/image/upload/v1611415440/logo_blue_k8rcyp.png"
+          alt=""
+          style={{ zIndex: "-1", width: "60px" }}
+        />
+        <CartSlide style={{ position: "fixed" }} />
+        <div className="menu-icon" >
+          <i
+            style={{ color: "black", width: "75px", height: "75px" }}
+            className={clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul id="MenuItems" className={clicked ? "nav-menu active" : "nav-menu"}>
+          <Link to="/">
+            <li className="nav-links">Home</li>
+          </Link>
+          <Link to="/store">
+            <li className="nav-links">Store</li>
+          </Link>
+
+          {sessionStorage.getItem("auth") === "true" ? (
             <li className="nav-links">
-              {sessionStorage.getItem("auth") === "false" ||
-              !sessionStorage.getItem("auth") ? (
-                <Link to="/login">Login</Link>
-              ) : (
-                <li>
-                  <form>
-                    <button
-                      type="submit"
-                      className="nav-links"
-                      value="Log In"
-                      style={{
-                        display: "inline-block",
-                        paddingTop: "5px",
-                        color: "black",
-                        height: "70px",
-                        position: "relative",
-                        top: "-25px",
-                        border: "1px solid black",
-                        borderRadius: "5px",
-                      }}
-                      onClick={logout}
-                    >
-                      Logout
-                    </button>
-                  </form>
-                </li>
-              )}
+              <Link to="/me">My past orders</Link>
             </li>
-          </ul>
-        </nav>
+          ) : (
+            ""
+          )}
+          {sessionStorage.getItem("admin") === "true" ? (
+            <li className="nav-links">
+              <Link to="/manage">Manage</Link>
+            </li>
+          ) : (
+            ""
+          )}
+          <li className="nav-links">
+            {sessionStorage.getItem("auth") === "false" ||
+            !sessionStorage.getItem("auth") ? (
+              <Link to="/login">Login</Link>
+            ) : (
+              <li>
+                <form>
+                  <button
+                    type="submit"
+                    className="nav-links"
+                    value="Log In"
+                    style={{
+                      display: "inline-block",
+                      paddingTop: "5px",
+                      color: "black",
+                      height: "70px",
+                      position: "relative",
+                      top: "-25px",
+                      border: "1px solid black",
+                      borderRadius: "5px",
+                    }}
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </form>
+              </li>
+            )}
+          </li>
+        </ul>
+      </nav>
 
-        {/* <img src={defaultImage} alt="" width="30px" height="30px" />
+      {/* <img src={defaultImage} alt="" width="30px" height="30px" />
               <button onClick={this.errorToggle}>
                 <img src="assets/shoes1.jpg" alt="" class="menu-icon" />
               </button> */}
-        {/* </div>
+      {/* </div>
           </div>
         </div> */}
-        <Switch>
-          {/* <Route path={"/"} render={(props) => (
+      <Switch>
+        {/* <Route path={"/"} render={(props) => (
             <Nav {...props} handleLogin={this.handleLogin} user={this.state.user} />
             )}>
             </Route> */}
-          <Route exact path={"/"}>
-            <Welcome />
-            <Home />
-          </Route>
-          <Route path="/store">
-            <Store />
-            {/* <Welcome />
+        <Route exact path={"/"}>
+          <Welcome />
+          <Home />
+        </Route>
+        <Route path="/store">
+          <Store />
+          {/* <Welcome />
             <Home /> */}
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route
-            path={"/login"}
-            render={(props) => (
-              <Login
-                {...props}
-                handleLogin={handleLogin}
-                loggedIn={loggedIn}
-              />
-            )}
-          ></Route>
-          <Route path="/me">
-            <Me />
-          </Route>
-          <Route
-            path={"/register"}
-            render={(props) => (
-              <Register
-                {...props}
-                handleLogin={handleLogin}
-                loggedIn={loggedIn}
-              />
-            )}
-          ></Route>
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route
+          path={"/login"}
+          render={(props) => (
+            <Login {...props} handleLogin={handleLogin} loggedIn={loggedIn} />
+          )}
+        ></Route>
+        <Route path="/me">
+          <Me />
+        </Route>
+        <Route
+          path={"/register"}
+          render={(props) => (
+            <Register
+              {...props}
+              handleLogin={handleLogin}
+              loggedIn={loggedIn}
+            />
+          )}
+        ></Route>
 
-          <Route path="/manage">
-            <Manage />
-          </Route>
-          <Route path="/allOrders">
-            <PastOrders />
-          </Route>
-          <Route path="/add">
-            <Add />
-          </Route>
-          <Route path="/orderProducts/:id">
-            <OrderProducts />
-          </Route>
+        <Route path="/manage">
+          <Manage />
+        </Route>
+        <Route path="/allOrders">
+          <PastOrders />
+        </Route>
+        <Route path="/add">
+          <Add />
+        </Route>
+        <Route path="/orderProducts/:id">
+          <OrderProducts />
+        </Route>
 
-          <Route path="/edit/:id" children={<Edit />}></Route>
-          <Route path="/order" children={<Order />}></Route>
-          <Route
-            path="/refundProducts/:id"
-            children={<RefundProducts />}
-          ></Route>
+        <Route path="/edit/:id" children={<Edit />}></Route>
+        <Route path="/order" children={<Order />}></Route>
+        <Route path="/refundProducts/:id" children={<RefundProducts />}></Route>
 
-          <Route path="/product/:id" children={<Product />}></Route>
-        </Switch>
-      </Router>
-    );
-}
+        <Route path="/product/:id" children={<Product />}></Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default Nav
+export default Nav;

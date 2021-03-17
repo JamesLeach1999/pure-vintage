605194c26ab967318beb731673946ec5e1ce64ec
james_leach1999 <james.leach1999@gmail.com>
2021-03-17 14:45:25 +0000

    testing admin page

--- work/src/pages/admin/AdminPage.js
+++ work/src/pages/admin/AdminPage.js
@@ -3,12 +3,12 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { createHashHistory } from "history";
 import { Admin, Resource } from "react-admin";
-import restProvider from "ra-data-simple-rest";
+import restProvider from "ra-data-json-server"
 // import defaultMessages from "ra-language-english";
 // import polyglotI18nProvider from "ra-i18n-polyglot";
 
 import createAdminStore from "./createAdminStore";
-import ProductList from "./ProductList";
+import ProductList from "./ProductList"
 // // your app components
 // import Dashboard from "./Dashboard";
 // import { PostList, PostCreate, PostEdit, PostShow } from "./Post";
@@ -16,33 +16,48 @@ import ProductList from "./ProductList";
 // import { UserList, UserEdit, UserCreate } from "./User";
 
 // dependency injection
-const dataProvider = restProvider("http://localhost:5000");
-
+const dataProvider = restProvider(
+  "http://localhost:5000"
+);
 const authProvider = () => Promise.resolve();
 
 const history = createHashHistory();
-console.log("numberwang1");
-const App = () => {
-  return (
-    <Provider
-      store={createAdminStore({
-        authProvider,
-        dataProvider,
-        history,
-      })}
+
+const App = () => (
+  <Provider
+    store={createAdminStore({
+      authProvider,
+      dataProvider,
+      history,
+    })}
+  >
+    <Admin
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      history={history}
+      title="My Admin"
     >
-      {console.log("numberwang2")}
-      <Admin
-        authProvider={authProvider}
-        dataProvider={dataProvider}
-        history={history}
-        title="My Admin"
-      >
-        {console.log("numberwang3")}
-        <Resource name="posts" list={ProductList} />
-      </Admin>
-    </Provider>
-  );
-};
+      <Resource
+        name="posts"
+        list={ProductList}
+        // create={PostCreate}
+        // edit={PostEdit}
+        // show={PostShow}
+      />
+      {/* <Resource
+        name="comments"
+        list={CommentList}
+        edit={CommentEdit}
+        create={CommentCreate}
+      />
+      <Resource
+        name="users"
+        list={UserList}
+        edit={UserEdit}
+        create={UserCreate}
+      /> */}
+    </Admin>
+  </Provider>
+);
 
 export default App;
